d8c4f48cf9fce49a2923631eaaa658d5c9c6c1c2
Joel Prentiss <joel.prentiss89@gmail.com>
2022-07-23 21:24:40 -0500

    edit

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db.json')
+const notes = require('../../../db/db.json');
+const {readFromFile, writeToFile, readAndAppend} = require('../helpers/fsUtils');
 
 // const PORT = process.env.PORT || 3001
 const PORT = 3001;
@@ -12,17 +13,27 @@ app.use(express.json());
 app.use(express.static('assets'));
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'index.html'));
+        res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/notes', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, readFromFile(notes)));
 });
 
-app.get('/api/notes', (req, res)=>
-    res.json(notes));
+app.get('/api/notes', (req, res)=>{
+    console.log(req)
+    readFromFile(req)
+    .then(notes => (
+        res.status(200).json(notes)
+    ))});
+    .catch(error) =>{
+        res.status(404).json(notes)
+    };
+    // });
+    
 
 app.post('/api/notes', (req, res)=>{
+    writeToFile
     res.json(`${req.method} request recieved, ${req.body}`);
 });    
 
